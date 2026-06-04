a38b235641d1a6ed2f320eba9c28585b5372aa91
Damar S.K <damarsyahada12@gmail.com>
2026-06-04 16:10:11 +0700

    feat: update dev script to use ts-node-dev and change server log message

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from "./app";
 const POST = process.env.PORT || 3000;
 
 app.listen(POST, () => {
-  console.log(`Server is running on port ${POST}`);
+  console.log(`Server is running on http://localhost:${POST}`);
 });
