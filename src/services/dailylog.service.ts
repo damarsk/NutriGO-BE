22f7e7a3df4e9e21f6591a4051bf59baf42a3ec0
damarsk <damarsyahada12@gmail.com>
2026-06-05 09:15:59 +0700

    feat: make nutritional fields optional in DailyLog model and related interfaces

--- src/services/dailylog.service.ts
+++ src/services/dailylog.service.ts
@@ -7,11 +7,11 @@ interface ICreateLog {
   mealTime: "BREAKFAST" | "LUNCH" | "DINNER" | "SNACK";
   consumtionGram: number;
   totalCalories: number;
-  totalProtein: number;
-  totalCarbs: number;
-  totalFat: number;
-  totalSugar: number;
-  totalNatrium: number;
+  totalProtein?: number;
+  totalCarbs?: number;
+  totalFat?: number;
+  totalSugar?: number;
+  totalNatrium?: number;
 }
 
 export const createLog = async (data: ICreateLog) => {
